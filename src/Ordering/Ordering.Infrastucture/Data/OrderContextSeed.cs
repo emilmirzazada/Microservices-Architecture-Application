3ddd3ef6d4896c401807a97914d7aac5019178c0
emilmirzazade <emildev09@gmail.com>
2021-10-27 12:49:49 +0400

    first commit

---
+++ src/Ordering/Ordering.Infrastucture/Data/OrderContextSeed.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastucture.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILoggerFactory loggerFactory, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            try
+            {
+                orderContext.Database.Migrate();
+
+                if (!orderContext.Orders.Any())
+                {
+                    orderContext.Orders.AddRange(GetPreConfiguredOrders());
+                    await orderContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception exception)
+            {
+                if (retryForAvailability<50)
+                {
+                    retryForAvailability++;
+                    var log = loggerFactory.CreateLogger<OrderContextSeed>();
+                    log.LogError(exception.Message);
+                    System.Threading.Thread.Sleep(2000);
+                    await SeedAsync(orderContext, loggerFactory, retryForAvailability);
+                }
+            }
+        }
+
+        private static IEnumerable<Order> GetPreConfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order(){UserName="swn",FirstName="Emil",LastName="Mirzazada",EmailAddress="emildev09@gmail.com",AddressLine="Bina",Country="Azerbaijan"}
+            };
+        }
+    }
+}
